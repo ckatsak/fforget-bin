11569a23f73c12558fd3973a544dca8de61bac99
Christos Katsakioris <ckatsak@gmail.com>
2022-01-30 05:18:11 +0200

    Fix logic; v0.0.2
    Signed-off-by: Christos Katsakioris <ckatsak@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
+// Test failure:
+//
+//     $ ./target/release/fforget /proc/cmdline
+//     Error: file '/proc/cmdline' still resides in page cache (possibly partially)
+//
+//     Caused by:
+//         0: mmap(2) failed
+//         1: Invalid argument (os error 22)
+//
+//  and
+//
+//     $ ./target/release/fforget /proc/slabinfo
+//     Error: failed to fforget file '/proc/slabinfo'
+//
+//     Caused by:
+//         0: Failed to open(2) file "/proc/slabinfo"
+//         1: Permission denied (os error 13)
+
 use std::path::PathBuf;
 
-use anyhow::{Context, Result};
+use anyhow::{bail, Context, Result};
 use clap::Parser;
 use fforget::{fcached, fforget};
 
@@ -16,32 +34,14 @@ fn main() -> Result<()> {
         fforget(path).with_context(|| format!("failed to fforget file '{}'", path.display()))?;
     }
     for path in cli.files.iter() {
-        fcached(path).with_context(|| {
-            format!(
-                "file '{}' still resides in page cache (possibly partially)",
+        if fcached(path)
+            .with_context(|| format!("failed to check if file '{}' still cached", path.display()))?
+        {
+            bail!(
+                "file '{}' still resides in the page cache (possibly partially)",
                 path.display()
-            )
-        })?;
+            );
+        }
     }
     Ok(())
 }
-
-/*
- * Test failure:
- *
- *     $ ./target/release/fforget-bin /proc/cmdline
- *     Error: file '/proc/cmdline' still resides in page cache (possibly partially)
- *
- *     Caused by:
- *         0: mmap(2) failed
- *         1: Invalid argument (os error 22)
- *
- *  and
- *
- *     $ ./target/release/fforget-bin /proc/slabinfo
- *     Error: failed to fforget file '/proc/slabinfo'
- *
- *     Caused by:
- *         0: Failed to open(2) file "/proc/slabinfo"
- *         1: Permission denied (os error 13)
- */
