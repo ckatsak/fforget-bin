2c3b62d7ebcee72e6d25e99dcc0d193d10fb783a
Christos Katsakioris <ckatsak@gmail.com>
2022-01-31 16:45:41 +0200

    v0.0.3: Add flags '--no-check' and '--check-only'
    Signed-off-by: Christos Katsakioris <ckatsak@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # fforget-bin
 
-Simple command line utility to hint Linux to remove the provided files from the
-page cache.
+Simple command line utility that attempts to hint Linux to remove the files at
+the provided paths from the page cache, and then (optionally, but enabled by
+default) checks if they still reside in memory (only performing the check is
+also possible).
+
+Check the built-in `--help` menu for more information.
+
+## Install
+
+Using [cargo](https://rustup.rs/) (from GitHub):
+
+```console
+$ cargo install --git https://github.com/ckatsak/fforget-bin
+```
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,65 @@
-// Test failure:
-//
-//     $ ./target/release/fforget /proc/cmdline
-//     Error: file '/proc/cmdline' still resides in page cache (possibly partially)
-//
-//     Caused by:
-//         0: mmap(2) failed
-//         1: Invalid argument (os error 22)
-//
-//  and
-//
-//     $ ./target/release/fforget /proc/slabinfo
-//     Error: failed to fforget file '/proc/slabinfo'
-//
-//     Caused by:
-//         0: Failed to open(2) file "/proc/slabinfo"
-//         1: Permission denied (os error 13)
-
 use std::path::PathBuf;
 
 use anyhow::{bail, Context, Result};
-use clap::Parser;
+use clap::{IntoApp, Parser};
 use fforget::{fcached, fforget};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
+    /// Paths to the files to attempt to remove from the page cache.
     files: Vec<PathBuf>,
+
+    /// Skip the final check on whether files still reside in memory.
+    ///
+    /// This flag can be used to fforget on a best-effort basis, without inducing a non-zero exit
+    /// status (and an accompanying error message on stderr) in case of failures.
+    #[clap(short, long)]
+    no_check: bool,
+
+    /// Only check whether the files at the provided paths appear to be in the page cache, without
+    /// hinting Linux to remove them if they do.
+    #[clap(short, long)]
+    check_only: bool,
 }
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
-    for path in cli.files.iter() {
-        fforget(path).with_context(|| format!("failed to fforget file '{}'", path.display()))?;
+    if cli.check_only && cli.no_check {
+        Cli::into_app()
+            .error(
+                clap::ErrorKind::ArgumentConflict,
+                "Flags '--check-only' and '--no-check' cannot coexist",
+            )
+            .exit();
     }
-    for path in cli.files.iter() {
-        if fcached(path)
-            .with_context(|| format!("failed to check if file '{}' still cached", path.display()))?
-        {
+
+    if !cli.check_only {
+        cli.files.iter().try_for_each(|path| {
+            fforget(path).with_context(|| format!("failed to fforget file '{}'", path.display()))
+        })?;
+    }
+
+    if !cli.no_check {
+        let mut still_cached = Vec::with_capacity(cli.files.len());
+        for path in cli.files.iter() {
+            let cached = fcached(path).with_context(|| {
+                format!(
+                    "failed to check whether file '{}' still resides in memory",
+                    path.display()
+                )
+            })?;
+            if cached {
+                still_cached.push(path);
+            }
+        }
+        if !still_cached.is_empty() {
             bail!(
-                "file '{}' still resides in the page cache (possibly partially)",
-                path.display()
+                "the following files still appear to reside in memory (possibly partially): {:#?}",
+                still_cached,
             );
         }
     }
+
     Ok(())
 }
